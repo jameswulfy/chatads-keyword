effeed712a99fa6ea1a82c5e76d2d7a28c166824
James Wulfenstein <wulfensteinjames@gmail.com>
2014-08-23 12:54:16 -0600

    add node.js

---
+++ MyApp/node.js
@@ -0,0 +1,22 @@
+var http = require('http');
+
+var Twitter = require('node-twitter');
+
+var twitterSearchClient = new Twitter.SearchClient(
+    'CONSUMER_KEY',
+    'CONSUMER_SECRET',
+    'TOKEN',
+    'TOKEN_SECRET'
+);
+
+twitterSearchClient.search({'q': 'node.js'}, function(error, result) {
+    if (error)
+    {
+        console.log('Error: ' + (error.code ? error.code + ' ' + error.message : error.message));
+    }
+
+    if (result)
+    {
+        console.log(result);
+    }
+});
